9d53f97b422c53b967dda3c2fc8460ec5ed1fcba
Benjamin Barber <bbarber@uchicago.edu>
2017-09-03 15:43:26 -0500

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
   <head>
     <link rel=”stylesheet” type=”text/css” href=“resources/styles.css” />    
   </head><body>
+    <?php include("resources/header.html")?>
     <h1>Hello World</h1>
   </body>
 </html>
